c311507425f15f13e861ab3ed7e20fb3600e3d6a
aogp1975 <2193038392@correo.ler.uam.mx>
2025-07-08 13:18:20 -0600

    fastify 5

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ fastify.register(require('@fastify/cors'), {
 });
 
 // Setup our static files
-fastify.register(require("@fastify/static"), {
+fastify.register(require('@fastify/static'), {
   root: path.join(__dirname, "public"),
   prefix: "/" // optional: default '/'
 });
@@ -115,11 +115,15 @@ fastify.post("/", function(request, reply) {
 });
 
 // Run the server and report out to the logs
-fastify.listen(process.env.PORT, '0.0.0.0', function(err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  console.log(`Your app is listening on ${address}`);
-  fastify.log.info(`server listening on ${address}`);
-});
+const start = async () => {
+	try {
+		const address = await fastify.listen ({ port: process.env.PORT || 3000, host: '0.0.0.0' });
+		console.log(`Your app is listening on ${address}`);
+	} catch (err) {
+		console.error('Error al iniciar el servidor:', err);
+		process.exit(1);
+	}
+};
+
+start ();
+
